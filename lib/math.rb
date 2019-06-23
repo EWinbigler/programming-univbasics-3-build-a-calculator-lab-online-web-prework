bce06b117fd00c68a0c50bfc120b79403d79b045
Eric Winbigler <uzof99@gmail.com>
2019-06-23 21:07:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+num1=10 - num2=5
 end
 
 def division(num1, num2)
-
+num1=50 / num2=2
 end
 
 def multiplication(num1, num2)
-
+num1=4 * num2=30
 end
 
 def modulo(num1, num2)
-
+num1=34.0 + num2=5
 end
 
 def square_root(num)
-
+num1=5 + num2=4
 end
